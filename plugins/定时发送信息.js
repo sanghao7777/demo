ac41bf07f4acfcf4894020b0d105d43f90d83c8b
sanghao7777 <sanghao3156@gmail.com>
2024-09-03 01:13:36 +0800

    Update 定时发送信息.js

--- plugins/定时发送信息.js
+++ plugins/定时发送信息.js
@@ -1,23 +1,22 @@
 /**
  * @author Aming
- * @name 定时任务
+ * @name 定时发送信息
  * @team Bncr团队
  * @version 1.0.0
- * @description 定时触发命令示例
+ * @description 定时发送信息
  * @priority 5
  * @disable false
  * @service true
  * @public false
- * @classification ["定时任务","示例插件"]
  */
-
+//cron定时规则
 const systemInfo = `
 系统正常
 `;
 sysMethod.cron.newCron('0 0 0,6,12,18 * * *', () => {
     sysMethod.push({
-        platform: 'tgBot',
-        userId: `1632046106`,
+        platform: 'tgBot',    //发送的平台
+        userId: `1632046106`, //个人id
         msg: systemInfo,
         type: 'text',
     }); 
